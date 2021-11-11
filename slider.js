70d68f73fa6da21d6edac73bb0f661bed174d380
Neeraj Shukla <neerajshukla8102@gmail.com>
2021-11-11 08:31:00 +0530

    fixed the slider and add it into home page

--- slider.js
+++ slider.js
@@ -5,8 +5,8 @@ var imgs=["https://mednear.com/uploads/banner/261be6779af16feb1bafadfe5227db75.j
 "https://mednear.com/uploads/banner/f6d37d929c75bd8f4e13e331513285a7.jpg",
 "https://mednear.com/uploads/banner/b4659e11d6d80df022d814e46018741e.jpg"
 ]
-document.getElementById("right").addEventListener("click",rightImg);
-document.getElementById("left").addEventListener("click",leftImg);
+document.getElementById("sl-div").addEventListener("click",rightImg);
+document.getElementById("sr-div").addEventListener("click",leftImg);
 function rightImg(){
     if(i>3){
         i=0;
@@ -14,7 +14,7 @@ function rightImg(){
     if(i<0){
         i=3;
     }
-    var img=document.querySelector("img");
+    var img=document.querySelector("#sl-img");
     console.log(i)
 
     img.setAttribute("src",imgs[i++])
@@ -28,7 +28,7 @@ function leftImg(){
     if(i<0){
         i=3;
     }
-    var img=document.querySelector("img");
+    var img=document.querySelector("#sl-img");
     console.log(i)
 
     img.setAttribute("src",imgs[i--])
